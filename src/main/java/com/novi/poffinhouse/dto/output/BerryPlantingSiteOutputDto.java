5718cf079ce8fc236cd0b42c8b5c8420ea3de55f
Bridoppp <tini.pham99@gmail.com>
2024-07-25 15:27:16 +0200

    teksgit remote add origin git@github.com:Birdoppp/poffinhouse.git

---
+++ src/main/java/com/novi/poffinhouse/dto/output/BerryPlantingSiteOutputDto.java
@@ -0,0 +1,4 @@
+package com.novi.poffinhouse.dto.output;
+
+public class BerryPlantingSiteOutputDto {
+}
---
+++ src/main/java/com/novi/poffinhouse/repositories/TeamRepository.java
@@ -0,0 +1,4 @@
+package com.novi.poffinhouse.repositories;
+
+public interface TeamRepository {
+}
