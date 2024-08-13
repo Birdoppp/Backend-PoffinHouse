e71010c9fc0668675af5d9bb0b7a411457d76305
Bridoppp <tini.pham99@gmail.com>
2024-08-13 16:30:37 +0200

    OwnedPokemon Delete endpoint fixed by adding cascade.remove to the OwnedPokemon relation

--- src/main/java/com/novi/poffinhouse/models/pokemon/Pokemon.java
+++ src/main/java/com/novi/poffinhouse/models/pokemon/Pokemon.java
@@ -51,7 +51,7 @@ public class Pokemon {
     private Integer speed;
 
 
-    @OneToMany
+    @OneToMany(mappedBy = "pokemon", cascade = CascadeType.REMOVE)
     private List<OwnedPokemon> ownedPokemonList;
 
 
