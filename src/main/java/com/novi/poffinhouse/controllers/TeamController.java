72de1f36b8eaf5b81697a1c7aac5da546f2c7fd2
Bridoppp <tini.pham99@gmail.com>
2024-08-30 19:24:00 +0200

    Team exists of OwnedPokemon and has AdjustPokemonTnTeamDto

--- src/main/java/com/novi/poffinhouse/controllers/TeamController.java
+++ src/main/java/com/novi/poffinhouse/controllers/TeamController.java
@@ -1,43 +1,37 @@
-//package com.novi.poffinhouse.controllers;
-//
-//import com.novi.poffinhouse.models.pokemon.Pokemon;
-//import com.novi.poffinhouse.models.pokemon.Team;
-//import com.novi.poffinhouse.services.TeamService;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/teams")
-//public class TeamController {
-//    private final TeamService teamService;
-//
-//    public TeamController(TeamService teamService) {
-//        this.teamService = teamService;
-//    }
-//
-//    @GetMapping
-//    public List<Team> getAllTeams() {
-//        return teamService.getAllTeams();
-//    }
-//
-//    @GetMapping("/{id}")
-//    public Team getTeamById(@PathVariable int id) {
-//        return teamService.getTeamById(id);
-//    }
-//
-//    @PostMapping
-//    public Team createTeam(@RequestBody Team team) {
-//        return teamService.saveTeam(team);
-//    }
-//
-//    @PostMapping("/{teamId}/pokemon")
-//    public Team addPokemonToTeam(@PathVariable int teamId, @RequestBody Pokemon pokemon) {
-//        return teamService.addPokemonToTeam(teamId, pokemon);
-//    }
-//
-//    @DeleteMapping("/{teamId}/pokemon/{pokemonId}")
-//    public void removePokemonFromTeam(@PathVariable int teamId, @PathVariable int pokemonId) {
-//        teamService.removePokemonFromTeam(teamId, pokemonId);
-//    }
-//}
+package com.novi.poffinhouse.controllers;
+
+import com.novi.poffinhouse.dto.input.TeamInputDto;
+import com.novi.poffinhouse.dto.output.TeamOutputDto;
+import com.novi.poffinhouse.dto.input.AdjustPokemonInTeamDto;
+import com.novi.poffinhouse.services.TeamService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/teams")
+public class TeamController {
+
+    private final TeamService teamService;
+
+    public TeamController(TeamService teamService) {
+        this.teamService = teamService;
+    }
+
+    @PostMapping
+    public ResponseEntity<TeamOutputDto> createTeam(@RequestBody TeamInputDto dto) {
+        TeamOutputDto teamOutputDto = teamService.createTeam(dto);
+        return ResponseEntity.ok(teamOutputDto);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TeamOutputDto> getTeamById(@PathVariable Long id) {
+        TeamOutputDto teamOutputDto = teamService.getTeamById(id);
+        return ResponseEntity.ok(teamOutputDto);
+    }
+    @PutMapping("/{teamId}/pokemon")
+    public ResponseEntity<TeamOutputDto> adjustPokemonInTeam(@PathVariable Long teamId, @RequestBody AdjustPokemonInTeamDto dto) {
+        TeamOutputDto teamOutputDto = teamService.adjustPokemonInTeam(teamId, dto);
+        return ResponseEntity.ok(teamOutputDto);
+    }
+
+}
